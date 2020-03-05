e4278537355e826a0a8c97ad858b45b0bc66a4e2
Idan Arye <idanarye@weka.io>
2020-03-05 14:59:56 +0200

    Kotlin use Groovy DSL

--- kotlin/hooks/post_gen_project.sh
+++ kotlin/hooks/post_gen_project.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-gradle init --type kotlin-application --dsl kotlin < /dev/null
+gradle init --type kotlin-application --dsl groovy < /dev/null
 rm src/test/kotlin/AppTest.kt
 cat > src/main/kotlin/App.kt <<-EOF
 fun main(args: Array<String>) {
